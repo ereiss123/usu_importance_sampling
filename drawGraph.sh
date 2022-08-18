ab83c896bdbf68698552abc21932bfb29ec6d797
Eric Reiss <reiss_eric@hotmail.com>
2022-08-18 17:39:42 -0600

    small fix

--- drawGraph.sh
+++ drawGraph.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMG_DIR = "./img/"
+#IMG_DIR = "./img/"
 echo "Checking depencies"
 if type prism &> /dev/null; then
     echo "prism is installed"
@@ -15,17 +15,17 @@ else
     sudo apt install graphviz
 fi
 
-if [ -d "$IMG_DIR" ]; then
+if [ ! -d "./img/" ]; then
     echo "Making image directory"
     mkdir ./img
 fi
 
 for i in *.prism; do
-    [ -f "$i" ] || echo "no prism file found";break
     x=$(basename $i .prism)
+    echo "x=$x"
     prism $i -exportmodel $i.dot
     dot -Tsvg $i.dot -o $x.svg
-    mv *.svg ./img
+    mv *.svg ./img/
     rm *.dot
     echo "$x.svg placed in ./img/"
 done
